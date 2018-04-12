4f09a1fe4d849c9d3711024022b898f51d3d2476
jx <jiangxin@coamc.com.cn>
2018-04-12 15:04:37 +0800

    Initial Commit

---
+++ RUAdView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RUAdView
+//
+//  Created by russ on 2018/4/12.
+//  Copyright © 2018年 russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RUAdView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RUAdView
+//
+//  Created by russ on 2018/4/12.
+//  Copyright © 2018年 russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
